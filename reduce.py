4ecaa16c3faf10325a7275700f127ba97f8d1e55
Olivier R-D <olivier.roulet@gmail.com>
2014-11-14 17:27:26 +0100

    cleanup

--- reduce.py
+++ reduce.py
@@ -19,36 +19,49 @@ class Reduce(object):
         self.compress_table(temp_table_name)
         self.compress_table(hum_table_name)
 
+    def get_oldest_timestamp(self, tablename):
+        result = self.db.execute("SELECT timestamp FROM '{}' ORDER BY rowid ASC LIMIT 1".format(tablename))
+        ts = result.fetchone()[0]
+        print ("oldest timestamp is : ", ts)
+        oldest = dateutil.parser.parse(ts)
+        return datetime.datetime(year=oldest.year, month=oldest.month, day=oldest.day, hour=oldest.hour) #rounding to hour
+
+
     def compress_table(self, tablename):
-        result = self.db.execute("SELECT rowid FROM '{}'".format(tablename))
+        #result = self.db.execute("SELECT rowid FROM '{}'".format(tablename))
         #if result.count() < 100:
             #print("table {} is probably just noise, removing".format(tablename))
             #return
 
         history_table = tablename + "_hours"
         self.check_or_create_table(history_table)
-        result = self.db.execute("SELECT timestamp FROM '{}' ORDER BY rowid ASC LIMIT 1".format(tablename))
-        ts = result.fetchone()[0]
-        print ("oldest timestamp is : ", ts)
-        oldest = dateutil.parser.parse(ts)
-        oldest = datetime.datetime(year=oldest.year, month=oldest.month, day=oldest.day, hour=oldest.hour) #rounding to hour
+
+        oldest = self.get_oldest_timestamp(tablename)
 
         now = datetime.datetime.now() 
-        end = now - datetime.timedelta(days=3)
+        end = now - datetime.timedelta(hours=2)
         start = oldest
         while start < end:
-            result = self.db.execute("SELECT rowid, * FROM '{}' WHERE datetime(timestamp) >= datetime('{}') AND datetime(timestamp) < datetime('{}')".format(tablename, start, start + datetime.timedelta(hours=1)))
-            temps = []
-            rowids = []
-            for rowid, ts, temp in result:
-                temps.append(temp)
-                rowids.append(rowid)
-            if len(temps) < 10:
-                print("not ennough data for ", start, " ignoring")
-                start += datetime.timedelta(hours=1)
-                continue
-            print("mean is: ", start, statistics.mean(temps))
+            rowids = self.mean_and_save(tablename, history_table, start, start + datetime.timedelta(hours=1))
+            self.remove_rows(tablename, rowids)
             start += datetime.timedelta(hours=1)
+
+    def mean_and_save(self, origintable, savetable, startts, endts):
+        result = self.db.execute("SELECT rowid, * FROM '{}' WHERE datetime(timestamp) >= datetime('{}') AND datetime(timestamp) < datetime('{}')".format(origintable, startts, endts))
+        temps = []
+        rowids = []
+        for rowid, ts, temp in result:
+            temps.append(temp)
+            rowids.append(rowid)
+        if len(temps) < 10:
+            print("not ennough data for ", startts, " ignoring")
+        else:
+            print("saving: ", savetable, startts, statistics.mean(temps))
+        return rowids
+
+    def remove_rows(self, tablename, rowids):
+        print("Removing ")#, rowids, " in ", tablename)
+
 
 if __name__ == "__main__":
     c = Reduce("sensordb.sql")
