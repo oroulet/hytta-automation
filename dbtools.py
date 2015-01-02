4a700645cd1caa612b9dfa0d4c732aa033cdeb4f
Olivier R-D <olivier.roulet@gmail.com>
2015-01-02 08:51:50 +0100

    add some tools

---
+++ dbtools.py
@@ -0,0 +1,102 @@
+import datetime 
+import dateutil.parser
+
+from IPython import embed
+import sqlite3
+
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
+
+class Tools(object):
+    def __init__(self):
+        self.dbpath = "sensordb.sql"
+        self.conn = None 
+        self.cur = None
+
+    def connect_db(self):
+        print("Connecting to db")
+        self.conn = sqlite3.connect(self.dbpath)
+        self.cur = self.conn.cursor()
+
+    def check_or_create_table(self, tablename):
+        res = self.cur.execute("select * from sqlite_master where name='%s'" % tablename).fetchall()
+        if not res:
+            print("creating table")
+            self.cur.execute("create table '%s' (timestamp STRING, value REAL)"%tablename)
+  
+    def get_table_list(self):
+        tables = []
+        for res in self.cur.execute("select name from sqlite_master"):
+            tables.append(res[0])
+        return tables
+
+    def get_count(self, tablename):
+        cmd = "SELECT COUNT(*) FROM '{}'".format(tablename)
+        return self.cur.execute(cmd).fetchone()[0]
+
+    def get_all_tables_count(self):
+        for name in self.get_table_list():
+            try:
+                print(name, ": ", self.get_count(name))
+            except:
+                print("Error reading table: ", name)
+
+    def remove_noise_tables(self, nb=5):
+        for name in self.get_table_list():
+            try:
+                count = self.get_count(name)
+                if count < nb: 
+                    cmd = "DROP TABLE '{}'".format(name)
+                    print(cmd)
+                    self.cur.execute(cmd)
+                else:
+                    print("keeping table: ", name)
+            except:
+                print("Error reading table: ", name)
+
+
+    def clean_table(self, tablename):
+        self.check_or_create_table(tablename + "_week")
+        self.check_or_create_table(tablename + "_all")
+        start = self.cur.execute("select min(datetime(timestamp)) from '{table}'".format(table=tablename)).fetchone()[0]
+        start = dateutil.parser.parse(start)
+        period = datetime.timedelta(minutes=10)
+        stop = datetime.datetime.now() - datetime.timedelta(minutes=10)
+
+        starttime = start
+        stoptime = start + period
+        cmd = "select avg(datetime(timestamp)) from '{table} where timestamp < {starttime} and timestamp {stoptime}'".format(table=tablename, starttime=starttime.isoformat(), stoptime=stoptime.isoformat())
+
+        return cmd 
+
+    def convert_all_to_epoch(self):
+        for tablename in self.get_table_list():
+            self.convert_to_epoch(tablename)
+
+    def convert_to_epoch(self, tablename):
+        cmd = "select rowid, timestamp from '{}'".format(tablename)
+        for rowid, ts in self.cur.execute(cmd).fetchall():
+            ts = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%f")
+            ts = unix_time(ts)
+            #cmd = "UPDATE '{}' SET timestamp={} WHERE rowid={}".format(tablename, ts, rowid)
+            print(cmd)
+            self.cur.execute(cmd)
+        
+
+     
+    def disconnect_db(self):
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
+
+if __name__ == "__main__":
+    t = Tools()
+    try:
+        t.connect_db()
+        table = t.get_table_list()[0]
+        embed()
+    finally:
+        t.disconnect_db()
--- reduce.py
+++ reduce.py
@@ -4,6 +4,10 @@ import time
 #import statistics
 #import dateutil.parser
 
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
 
 class Reduce(object):
     def __init__(self, dbpath):
@@ -24,11 +28,25 @@ class Reduce(object):
     def get_oldest_timestamp(self, tablename):
         result = self.db.execute("SELECT timestamp FROM '{}' ORDER BY rowid ASC LIMIT 1".format(tablename))
         ts = result.fetchone()[0]
-        print ("oldest timestamp is : ", ts)
+        print ("oldest timestamp in: ", tablename, " is : ", ts)
         #oldest = dateutil.parser.parse(ts)
         #oldest = ISO8601ToDateTime(ts)
         oldest = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%f")
         return datetime.datetime(year=oldest.year, month=oldest.month, day=oldest.day, hour=oldest.hour) #rounding to hour
+        #oldest -= datetime.timedelta(minutes=oldest.minute%10, seconds=oldest.second, microseconds=oldest.microsecond) # rundt to 10 minutes
+        #return oldest
+
+    def get_newest_timestamp(self, tablename):
+        result = self.db.execute("SELECT timestamp FROM '{}' ORDER BY rowid DESC LIMIT 1".format(tablename))
+        ts = result.fetchone()[0]
+        print ("newest timestamp in: ", tablename , " is : ", ts)
+        #oldest = dateutil.parser.parse(ts)
+        #oldest = ISO8601ToDateTime(ts)
+        oldest = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%f")
+        return datetime.datetime(year=oldest.year, month=oldest.month, day=oldest.day, hour=oldest.hour) #rounding to hour
+        #oldest -= datetime.timedelta(minutes=oldest.minute%10, seconds=oldest.second, microseconds=oldest.microsecond) # rundt to 10 minutes
+        #return oldest
+
 
 
     def compress_table(self, tablename):
@@ -43,29 +61,37 @@ class Reduce(object):
         oldest = self.get_oldest_timestamp(tablename)
 
         now = datetime.datetime.now() 
-        end = now - datetime.timedelta(hours=2)
+        #now -= datetime.timedelta(minutes=now.minute%10, seconds=now.second, microseconds=now.microsecond) # rundt to 10 minutes
+        now -= datetime.timedelta(minutes=now.minute, seconds=now.second, microseconds=now.microsecond) # rundt to hour
+        end = now - datetime.timedelta(hours=48)
         start = oldest
         while start < end:
-            rowids = self.mean_and_save(tablename, history_table, start, start + datetime.timedelta(hours=1))
+            rowids = self.mean_and_save(tablename, history_table, start, start + datetime.timedelta(minutes=10))
             self.remove_rows(tablename, rowids)
             start += datetime.timedelta(hours=1)
 
     def mean_and_save(self, origintable, savetable, startts, endts):
-        result = self.db.execute("SELECT rowid, * FROM '{}' WHERE datetime(timestamp) >= datetime('{}') AND datetime(timestamp) < datetime('{}')".format(origintable, startts, endts))
+        cmd = "SELECT rowid, * FROM '{}' WHERE datetime(timestamp) >= datetime('{}') AND datetime(timestamp) < datetime('{}')".format(origintable, startts, endts)
+        print(cmd)
+        result = self.db.execute(cmd)
         temps = []
         rowids = []
         for rowid, ts, temp in result:
             temps.append(temp)
             rowids.append(rowid)
+        print("Result temps are: ", temps)
         if len(temps) < 10:
             print("not ennough data for ", startts, " ignoring")
         else:
             mean = float(sum(temps))/len(temps)
-            print("saving: ", savetable, startts, mean)
+            cmd = "INSERT INTO '{}' values ({}, {})".format(savetable, unix_time(startts), mean)
+            print(cmd)
         return rowids
 
     def remove_rows(self, tablename, rowids):
-        print("Removing ")#, rowids, " in ", tablename)
+        for rowid in rowids:
+            cmd = "DELETE from '{}' WHERE rowid = {}".format(tablename, rowid)
+            print(cmd)
 
 
 if __name__ == "__main__":
